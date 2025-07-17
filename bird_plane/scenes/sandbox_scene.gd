ec437d6c2bd8a970de3ab7deb7ec6b2aaa6be182
Alf-André Walla <fwsgonzo@hotmail.com>
2025-07-17 21:08:44 +0200

    Update to v0.40 w/JIT support

--- bird_plane/scenes/sandbox_scene.gd
+++ bird_plane/scenes/sandbox_scene.gd
@@ -1,13 +1,13 @@
 extends Node3D
 
 @export var smooth_follow: Sandbox_BirdPlaneCoreSmoothFollowSmoothFollow
-@export var thrust: Sandbox_BirdPlaneCoreThrustThrust
-@export var turning: Sandbox_BirdPlaneCoreTurningTurning
+@export var thrust: CPPScript_BirdPlaneCoreThrustThrust
+@export var turning: CPPScript_BirdPlaneCoreTurningTurning
 
 var sensitivity: float
 var mouse_speed: Vector2
 
-func simple_method_filter(sandbox, obj, method: String) -> bool:
+func simple_method_filter(_sandbox, _obj, method: String) -> bool:
 	# TODO: Record the actual methods needed, dump them on exit and add them here
 	return method != "free" && method != "queue_free"
 
@@ -16,6 +16,12 @@ func _ready() -> void:
 	smooth_follow.set_method_allowed_callback(simple_method_filter)
 	thrust.set_method_allowed_callback(simple_method_filter)
 	turning.set_method_allowed_callback(simple_method_filter)
+	if smooth_follow.is_jit():
+		print("SmoothFollow program is JIT-compiled")
+	if thrust.is_jit():
+		print("Thrust program is JIT-compiled")
+	if turning.is_jit():
+		print("Turning program is JIT-compiled")
 
 	var fa = FileAccess.open("res://bintr.c", FileAccess.WRITE)
 	fa.store_string(thrust.emit_binary_translation(true, true))
